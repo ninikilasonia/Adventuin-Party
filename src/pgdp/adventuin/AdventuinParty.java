e57c0f2cc1b77810ce50bac2a6b8e9d9ac3c4d33
Nino Kilasonia <kilasonia.nino@kiu.edu.ge>
2023-12-15 20:59:20 +0400

    //

--- src/pgdp/adventuin/AdventuinParty.java
+++ src/pgdp/adventuin/AdventuinParty.java
@@ -1,5 +1,27 @@
 package pgdp.adventuin;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public final class AdventuinParty {
-    // TODO
+
+    public static Map<HatType, List<Adventuin>> groupByHatType(List<Adventuin> adventuins) {
+        Map<HatType, List<Adventuin>> res = new HashMap<>();
+        List<HatType> distHats = new ArrayList<>();
+
+        adventuins.forEach(x -> distHats.add(x.getHatType()));
+        distHats.stream().distinct();
+
+        distHats.forEach(x -> {
+            List<Adventuin> byHats = new ArrayList<>();
+            adventuins.stream().forEach(a -> {
+                if (x.equals(a.getHatType()) ) {
+                    byHats.add(a);
+                }
+            });
+        });
+        return res;
+    }
 }
